5fa69b7b034ebe404ab6b642f078b2fffce5f9e3
eric4wan <eric4wan@gmail.com>
2015-02-22 01:34:33 -0500

    Hopefully got it all working now

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -1,6 +1,6 @@
 //Eric Wan
-//A script I wrote for uh, no reason?
-var txt = "<p>Here's some of your info: </p>";
+//A script I wrote for uh, no reason? Just pokes at your platform
+var txt = "";
 var platform = navigator.platform;
 if (platform.match(/Linux/i)) {
   platform += " (nice!)";// because your above average CS major uses linux
