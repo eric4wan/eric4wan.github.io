d4d009e4d35f51fa0b25a0369b17547c93cacb30
eric4wan <eric4wan@gmail.com>
2015-02-22 02:12:16 -0500

    fixed layering syntax

--- javascripts/fav.js
+++ javascripts/fav.js
@@ -1,10 +1,16 @@
 //Eric Wan
 //A script I wrote to add this page to your favorites
 //no idea why anyone would but whatever
-if ('sidebar' in window  && 'addPanel' in window.sidebar) {//mozilla
-  window.sidebar.addPanel(location.href, document.title, "");
-} else if( /*@cc_on!@*/false) {//IE why would you willingly use IE 
-  window.external.AddFavorite(location.href, document.title);
-} else {//safari or chrome
-  alert('Press " + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + ' + D to boomark this page.');
-}
+(function() {
+  ('#bookmarkme').click(function() {
+    var url = "http://www.eric4wan.github.io";
+    var title = "Eric Wan's Website";
+    if ('sidebar' in window  && 'addPanel' in window.sidebar) {//mozilla
+      window.sidebar.addPanel(url, title, "");
+    } else if( /*@cc_on!@*/false) {//IE why would you willingly use IE 
+     window.external.AddFavorite(url, title);
+    } else {//safari or chrome
+     alert('Press " + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + ' + D to boomark this page.');
+    }
+  });
+});
