2fe6d85095215a016e714bc5b8330ec646c22e75
eric4wan <eric4wan@gmail.com>
2015-02-22 00:42:19 -0500

    Fixed js syntax issues

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -2,7 +2,7 @@
 var txt = "<p>Here's some of your info: </p>";
 var platform = navigator.platform;
 if (platform.match(/Linux/i)) {
-  platform =+ " (nice!)";// because your above average CS major uses linux
+  platform += " (nice!)";// because your above average CS major uses linux
 }
 txt += "<p>You are currently running " + platform + ".</p>;
 
