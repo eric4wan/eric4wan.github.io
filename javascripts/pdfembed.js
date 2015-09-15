8c40b32dd55bf545d7c149a09d883aa04e5458b8
Eric Wan <eric4wan@gmail.com>
2015-09-15 01:27:17 -0400

    trying pdfjs please just work

---
+++ javascripts/pdfembed.js
@@ -0,0 +1,19 @@
+'use strict';
+
+PDFJS.getDocument('files/EWanResume.pdf').then(function(pdf) {
+  pdf.getPage(1).then(function(page0 {
+    var scale = 1.5;
+    var viewport = page.getViewport(scale);
+
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    page.render(renderContext);
+  }));
+});
