2a888212a3108d398d5a9c71640d3e88bed570c8
Eric Wan <eric4wan@gmail.com>
2016-04-18 22:48:29 -0400

    testing papa

--- javascripts/cnj.js
+++ javascripts/cnj.js
@@ -1,31 +1,52 @@
-var c0 = c3.generate({
-    bindto: '#c0',
-    data: {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ],
-      axes: {
-        data2: 'y2' // ADD
-      }
-    },
-    axis: {
-      y2: {
-        show: true // ADD
-      }
-    }
+// var c0 = c3.generate({
+//     bindto: '#c0',
+//     data: {
+//       columns: [
+//         ['data1', 30, 200, 100, 400, 150, 250],
+//         ['data2', 50, 20, 10, 40, 15, 25]
+//       ],
+//       axes: {
+//         data2: 'y2' // ADD
+//       }
+//     },
+//     axis: {
+//       y2: {
+//         show: true // ADD
+//       }
+//     }
+// });
+//
+// c3.generate({
+//     bindto: '#c1',
+//     data: {
+//       columns: [
+//         ['data1', 5, 5, 6, 2, 5, 250]
+//       ]
+//     },
+//     axis: {
+//       y2: {
+//         show: true // ADD
+//       }
+//     }
+// });
+
+Papa.parse("http://eric4wan.github.io/output.csv", {
+	download: true,
+	complete: function(results) {
+    console.log("done");
+    console.log(results);
+    graphs(results);
+	}
 });
 
-c3.generate({
-    bindto: '#c1',
-    data: {
-      columns: [
-        ['data1', 5, 5, 6, 2, 5, 250]
-      ]
-    },
-    axis: {
-      y2: {
-        show: true // ADD
+function graphs(csvStr) {
+  bindStr = 'c';
+  for (var i = 0; i < 23; i++) {
+    c3.generate({
+      bindto: (bindStr + str(i)),
+      data: {
+
       }
-    }
-});
+    })
+  }
+}
