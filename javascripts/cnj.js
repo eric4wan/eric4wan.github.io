f72da2acf5a3d61a714b08f6eb2ce3b5310e5efd
Eric Wan <eric4wan@gmail.com>
2016-04-19 01:23:41 -0400

    please???

--- javascripts/cnj.js
+++ javascripts/cnj.js
@@ -1,34 +1,19 @@
-// var c0 = c3.generate({
-//     bindto: '#c0',
-//     data: {
-//       columns: [
-//         ['data1', 30, 200, 100, 400, 150, 250],
-//         ['data2', 50, 20, 10, 40, 15, 25]
-//       ],
-//       axes: {
-//         data2: 'y2' // ADD
-//       }
-//     },
-//     axis: {
-//       y2: {
-//         show: true // ADD
-//       }
-//     }
-// });
-//
-// c3.generate({
-//     bindto: '#c1',
-//     data: {
-//       columns: [
-//         ['data1', 5, 5, 6, 2, 5, 250]
-//       ]
-//     },
-//     axis: {
-//       y2: {
-//         show: true // ADD
-//       }
-//     }
-// });
+Array.prototype.SumArray = function (arr) {
+    var sum = [];
+    if (arr != null && this.length == arr.length) {
+        for (var i = 2; i < arr.length; i++) {
+            sum.push(this[i] + arr[i]);
+        }
+    }
+    return sum;
+}
+
+Array.prototype.DivFiddy = function () {
+  for (var i = 0; i < this.length; i++) {
+    this[i] = this[i] / 50;
+  }
+  return this;
+}
 
 Papa.parse("http://eric4wan.github.io/output.csv", {
 	download: true,
@@ -41,15 +26,18 @@ Papa.parse("http://eric4wan.github.io/output.csv", {
 function graphs(d) {
   bindStr = 'c';
   for (var i = 0; i < 23; i++) {
-    arrFiddy = [];
+    arrFiddy = d[1]
     for (var j = 1; j < 51; j++) {
-      arrFiddy.push(d[(i * 50 + j)]);
+      arrFiddy.SumArray(d[(i * 50 + j)]);
     }
+    arrFiddy = arrFiddy.DivFiddy();
     $('#graphs').append("<div id='" + i + "|" + j + "|" + 1 + "'></div>");
     c3.generate({
       bindto: (bindStr + i.toString()),
       data: {
-        url: 'http://eric4wan.github.io/output.csv'
+        columns: [
+          ['data'].concat(arrFiddy)
+        ]
       }
     });
   }
