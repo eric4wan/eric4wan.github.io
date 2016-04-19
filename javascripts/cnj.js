bc8a05dc9cd5f5bc59a1f553172840f204c8e187
Eric Wan <eric4wan@gmail.com>
2016-04-19 01:33:59 -0400

    fix

--- javascripts/cnj.js
+++ javascripts/cnj.js
@@ -1,7 +1,7 @@
 Array.prototype.SumArray = function (arr) {
     var sum = [];
     if (arr != null && this.length == arr.length) {
-        for (var i = 2; i < arr.length; i++) {
+        for (var i = 0; i < arr.length; i++) {
             sum.push(this[i] + arr[i]);
         }
     }
